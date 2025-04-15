f23fd288cfd6fcfb2aefc5cf1381d6c94554fa17
Gabi1818 <jaresova.gabinka@gmail.com>
2025-04-15 08:23:31 +0200

    error fix

--- app/api/animals/[id]/route.ts
+++ app/api/animals/[id]/route.ts
@@ -3,6 +3,7 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(
     req: NextRequest,
+    //{ params }: { params: { id: string } }
     { params }: { params: Promise<{ id: string }> }
 ) {
     const id = (await params).id;
